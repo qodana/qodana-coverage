ce25ca6bfd6bdc9a64e2abbfeed084b1ed48155c
hybloid <48032702+hybloid@users.noreply.github.com>
2023-07-05 15:33:42 +0200

    Workflow for PHP - PhpUnit case (#6)

--- PHP/phpunit/run.bat
+++ PHP/phpunit/run.bat
@@ -1,3 +1,3 @@
 @echo off
 set XDEBUG_MODE=coverage
-.\vendor\bin\phpunit --coverage-clover .\.qodana\coverage-data\coverage.xml --configuration phpunit.xml --filter FooTest --test-suffix FooTest.php
+.\vendor\bin\phpunit --coverage-clover .\.qodana\code-coverage\coverage.xml --configuration phpunit.xml --filter FooTest --test-suffix FooTest.php
