bfe4d603dc63c888a69bbf75eccea6c2f4d857a1
hybloid <48032702+hybloid@users.noreply.github.com>
2024-08-28 10:47:24 +0200

    .NET coverage (#20)
    * Provide example for .NET coverage
    
    * Remove coverlet from refs

---
+++ NET/coverlet/Class1.cs
@@ -0,0 +1,44 @@
+﻿namespace QodanaCoverageTest;
+
+public class Class1 : Interface1
+{
+    public Class1()
+    {
+        var y = 5;
+    }
+
+    public void UsedMethod()
+    {
+        LocalMethod();
+        void LocalMethod()
+        {
+            var x = 5;
+        }
+        void UnusedLocalMethod() { }
+    }
+
+    public void UsedMethodWithIf(int i)
+    {
+        if (i == 1)
+        {
+            Console.Write(i);
+        }
+        else
+        {
+            Console.Write(i);
+            Console.Write(i);
+        }
+    }
+
+    public int UsedLambdaMethod() => 4;
+    
+    public int UnusedLambdaMethod() => 5;
+
+    private void UnusedMethod()
+    {
+        void UnusedLocalMethod()
+        {
+            
+        }
+    }
+}
